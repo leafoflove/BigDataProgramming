39fff351e28a7cfd5587021c7d34c83ea444171a
Gaurav Nanda <gnanda@TACC-AirSpare02.local>
2015-02-08 22:59:18 -0600

    Some code comments and minor fixes.

--- src/main/java/com/refactorlabs/cs378/assign2/WordStatistics.java
+++ src/main/java/com/refactorlabs/cs378/assign2/WordStatistics.java
@@ -20,7 +20,8 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 /**
- * Tasks to be done in this homework.
+ * Word statistics class is used to compute the mean and variance for each word that appears in one or more paragraphs of the input document.
+ * We use Map-Reduce Paradigm to solve this problem.
  * 
  * @author Gaurav Nanda (nanda@utexas.edu)
  */
@@ -29,13 +30,14 @@ public class WordStatistics {
 	
 	public static class MapClass extends Mapper<LongWritable, Text, Text, LongArrayWritable> {
 		/**
-		 * Counter group for the mapper.  Individual counters are grouped for the mapper.
+		 * Counter group for the mapper. Individual counters are grouped for the mapper.
 		 */
 		private static final String MAPPER_COUNTER_GROUP = "Mapper Counts";
 		
 		/**
-		 * Create a private HashMap<String, Count>, so that we dont have to
-		 * create the hash object on every map() invocation.
+		 * Create a private HashMap<String, Count>, so that we don't have to
+		 * create the hash object on every map() invocation. However ensure
+		 * to clear the HashMap before using during every invocation.
 		 */
 		private static final HashMap<String, Long> wordCountMap = 
 				new HashMap<String, Long>();
@@ -46,7 +48,12 @@ public class WordStatistics {
 		private static final LongArrayWritable longArrayWritable =
 				new LongArrayWritable();
 		
-		private List<Character> punctuations = Arrays.asList('-', '_', '\"', ',', ';', ':', '\'', '.');
+		/**
+		 * These punctuation characters could not be a part of normal string tokenizer
+		 * as that would also split words like good-natured. However, we would have
+		 * to handle cases like "---good--". Similarly for "\'" also.
+		 */
+		private List<Character> punctuations = Arrays.asList('-', '\'');
 		
 		@Override
 		public void map(LongWritable mapKey, Text value, Context context)
@@ -56,7 +63,7 @@ public class WordStatistics {
 			String line = value.toString();
 			
 			// Tokenize the paragraph.
-			StringTokenizer tokenizer = new StringTokenizer(line, "=_\";:.,?[ ");
+			StringTokenizer tokenizer = new StringTokenizer(line, "=_\";:.,?[! ");
 			
 			// Count the number of paragraphs.
 			context.getCounter(MAPPER_COUNTER_GROUP, "Input Lines").increment(1L);
@@ -69,9 +76,8 @@ public class WordStatistics {
 			while(tokenizer.hasMoreTokens()) {
 				StringBuilder token = new StringBuilder(tokenizer.nextToken().toLowerCase());
 				
-				System.out.println(token);
-				
-				// Some code to strip off the words.
+				// Some code to strip off the words. This code would take
+				// care of stripping of punctuation from front and behind.
 				boolean anyChange = true;
 				while(token.length() > 0 && anyChange) {
 					anyChange = false;
@@ -86,25 +92,29 @@ public class WordStatistics {
 						anyChange = true;
 					}
 				}
+				
+				// If we ended up removing everything, then simply continue.
 				if (token.length() == 0) {
 					continue;
 				}
-			
-				if (token.charAt(token.length() - 1) == ']') {
-					token.insert(0, '[');
-				}
+
 				String word = token.toString();
-				
-				// Fill the word count map. If it is the first 
-				// occurrence, then set the count to be 1, otherwise
-				// increment count by 1.
-				if (wordCountMap.containsKey(word)) {
-					wordCountMap.put(word, wordCountMap.get(word) + 1);
+								
+				// This is to handle another weird case of "--", that comes between
+				// two words. Split that into two words and update the map for both.
+				if (word.contains("--")) {
+					String word1 = word.substring(0, word.indexOf("--"));
+					String word2 = word.substring(word.indexOf("--") + 2);
+					updateWordCountMap(word1);
+					updateWordCountMap(word1);
 				} else {
-					wordCountMap.put(word, 1L);
+					updateWordCountMap(word);
+
 				}
 			}
 			
+			// Iterate over the key in the HashMap and write the Long Array
+			// having 1L, count of the word and respective square of the values.
 			for (String key : wordCountMap.keySet()) {
 				long count = wordCountMap.get(key);
 				long squareCount = count * count;
@@ -114,10 +124,30 @@ public class WordStatistics {
 				context.getCounter(MAPPER_COUNTER_GROUP, "Words Out").increment(1L);
 			}
 		}
+	
+		/*
+		 * Fill the word count map. If it is the first occurrence, 
+		 * then set the count to be 1, otherwise  increment count by 1.
+		 */
+		private void updateWordCountMap(String word) {
+			// This is an one odd case, where words like precise[173]. 
+			// According to Professor, these two should be two different words.
+			// To Handle this, I am using "[" as string tokenizer, and later checking
+			// if the token has "]", if so, I add "[" back and retain entire [173].
+			if (word.charAt(word.length() - 1) == ']') {
+				word = "[" + word;
+			}
+				
+			if (wordCountMap.containsKey(word)) {
+				wordCountMap.put(word, wordCountMap.get(word) + 1);
+			} else {
+				wordCountMap.put(word, 1L);
+			}	
+		}
 	}
 	
 	/**
-	 * Combiner class that would combine <paragraph_count, avg, square_avg>.
+	 * Combiner class that would combine <paragraph_count, sum, square_sum>.
 	 */
 	public static class CombinerClass extends Reducer<Text, LongArrayWritable, Text, LongArrayWritable> {
 		/**
@@ -128,7 +158,7 @@ public class WordStatistics {
 		/**
 		 * Create one LongWritable object that could be reused.
 		 */
-		LongArrayWritable longArrayWritable = new LongArrayWritable();
+		private LongArrayWritable longArrayWritable = new LongArrayWritable();
 		
 		public void reduce(Text key, Iterable<LongArrayWritable> values, Context context)
 				throws IOException, InterruptedException {
@@ -140,6 +170,8 @@ public class WordStatistics {
 			long sum = 0;
 			long squaredSum = 0;
 			
+			// Combiner would simply sum up paragraph count, word count and its respective square count, 
+			// for the same key.
 			for (LongArrayWritable value: values) {
 				long[] arrayValues = value.getValueArray();
 				count += arrayValues[0];
@@ -169,7 +201,7 @@ public class WordStatistics {
 
 			// Sum up the counts for the current word, specified in object "key".
 			long count = 0L;
-			double mean = 0L;
+			double mean = 0;
 			double variance = 0;
 			for (LongArrayWritable value : values) {
 				long[] arrayValues = value.getValueArray();
